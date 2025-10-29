69c9c4284aec351051828dd970d548b3f30543e0
DoXuanManh-BN24V7x116 <manhbn24v7x116@dttx.ctu.edu.vn>
2025-10-29 14:59:22 +0700

    Cài đặt xử lý lỗi (app.js và api-error.js)

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/api-error");
+const contactsRouter = require("./app/routes/contact.route");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 // Đăng ký các route
-const contactsRouter = require("./app/routes/contact.route");
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào khớp với yêu cầu
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware (xử lý lỗi tập trung)
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
